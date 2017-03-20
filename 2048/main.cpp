d8bd166628db71113aeff2fcc62d415bde3f917d
Li Jiajie <ducky@lis-macbook-pro.local>
2017-03-20 20:49:58 +0800

    Initial Commit

---
+++ 2048/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2048
+//
+//  Created by Li Jiajie on 3/20/17.
+//  Copyright © 2017 Li Jiajie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
